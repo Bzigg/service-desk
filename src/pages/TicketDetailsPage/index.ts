f4b72b9b75c69cd1d97a6f8a6bacf94bed807ff8
plekhanovno <n.plekhanov@modulbank.ru>
2024-06-27 10:25:58 +0500

    правка путей файлов

--- src/pages/TicketDetailsPage/index.ts
+++ src/pages/TicketDetailsPage/index.ts
@@ -1,6 +1,6 @@
 export {
     TicketDetailsPageAsync as TicketDetailsPage,
-} from './ui/ArticleDetailsPage/TicketDetailsPage.async';
+} from 'pages/TicketDetailsPage/ui/TicketDetailsPage/TicketDetailsPage.async';
 
 export type { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
 export type { ArticleDetailsRecommendationsSchema } from './model/types/ArticleDetailsRecommendationsSchema';
