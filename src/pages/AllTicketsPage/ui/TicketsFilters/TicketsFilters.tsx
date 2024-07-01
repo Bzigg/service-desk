04c3f42d72bd074fa3fb4797e8729ff3a1ae52f0
plekhanovno <n.plekhanov@modulbank.ru>
2024-07-01 16:57:51 +0500

    настроил компонент фильтров

--- src/pages/AllTicketsPage/ui/TicketsFilters/TicketsFilters.tsx
+++ src/pages/AllTicketsPage/ui/TicketsFilters/TicketsFilters.tsx
@@ -1,42 +1,55 @@
-import React, { FC, useContext } from 'react'
+import React, { FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { Select } from 'shared/ui/Select/Select'
 import { Button } from 'shared/ui/Button/Button'
 
 interface IProps {
 	setFilters: any
 }
 
+const statusOptions = [
+	{
+		value: 'ALL',
+		content: 'Все заявки'
+	},
+	{
+		value: 'OPEN',
+		content: 'Открытые'
+	},
+	{
+		value: 'IN_PROGRESS',
+		content: 'В работе'
+	},
+	{
+		value: 'COMPLETED',
+		content: 'Выполненные'
+	},
+	{
+		value: 'REJECTED',
+		content: 'Отклоненные'
+	}
+]
+
 const TicketsFilters: FC<IProps> = ({ setFilters }) => {
+	const { control, handleSubmit } = useForm({
+		defaultValues: {
+			status: 'ALL'
+		}
+	})
 
 	return (
-		<div>
-			<Button onClick={() => setFilters(12)}>
-				123
+		<form onSubmit={handleSubmit(setFilters)}>
+			<Select
+				className="mt8 w180p"
+				control={control}
+				name="status"
+				options={statusOptions}
+				label="Статус заявки"
+			/>
+			<Button type="submit">
+				Применить
 			</Button>
-			{/*<div className={cls.sortWrapper}>*/}
-			{/*	<ArticleSortSelector*/}
-			{/*		order={order}*/}
-			{/*		sort={sort}*/}
-			{/*		onChangeOrder={onChangeOrder}*/}
-			{/*		onChangeSort={onChangeSort}*/}
-			{/*	/>*/}
-			{/*	<ArticleViewSelector*/}
-			{/*		view={view}*/}
-			{/*		onViewClick={onChangeView}*/}
-			{/*	/>*/}
-			{/*</div>*/}
-			{/*<Card className={cls.search}>*/}
-			{/*	<Input*/}
-			{/*		onChange={onChangeSearch}*/}
-			{/*		value={search}*/}
-			{/*		placeholder={t('Поиск')}*/}
-			{/*	/>*/}
-			{/*</Card>*/}
-			{/*<ArticleTypeTabs*/}
-			{/*	value={type}*/}
-			{/*	onChangeType={onChangeType}*/}
-			{/*	className={cls.tabs}*/}
-			{/*/>*/}
-		</div>
+		</form>
 	);
 };
 
