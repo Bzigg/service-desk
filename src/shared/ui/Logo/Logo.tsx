68e9dbdaee1cef42ac672c0897b24c64bdaa34a2
plekhanovno <n.plekhanov@modulbank.ru>
2024-07-02 15:23:17 +0500

    удалил лишнее

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Text, TextTheme } from 'shared/ui/Text/Text'
 import { AppLink } from 'shared/ui/AppLink/AppLink'
-import cls from './Logo.scss'
 
 export const Logo = () => (
 	<AppLink to="/">
 		<Text
-			className={cls.appName}
 			title="Service-Desk"
 			theme={TextTheme.INVERTED}
 		/>
