f7029b859cca57246c8ec3ad0be9bd11fab68b98
plekhanova <plekhanova@tochka.com>
2024-10-18 14:09:03 +0500

    BE получение одного и всех корпусов

--- backend/src/buildings/buildings.controller.ts
+++ backend/src/buildings/buildings.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common'
+import { Body, Controller, Get, Headers, Post, Query } from '@nestjs/common'
 import { BuildingsService } from './buildings.service'
 
 @Controller('buildings')
 export class BuildingsController {
 
-  constructor(private ticketsService: BuildingsService) {
+  constructor(private buildingsService: BuildingsService) {
+  }
+
+  @Get()
+  getBuildingById(@Query() query) {
+    return this.buildingsService.getBuildingById(query?.id)
+  }
+
+  @Get('/all')
+  getAll() {
+    return this.buildingsService.getAllBuildings()
   }
 
   @Post('/add')
   addBuilding(@Body() dto, @Headers('authorization') token: string) {
-    return this.ticketsService.addBuilding(dto, token)
+    return this.buildingsService.addBuilding(dto, token)
   }
 }
--- backend/src/buildings/buildings.service.ts
+++ backend/src/buildings/buildings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { AuthService } from '../auth/auth.service'
 import { Building } from './buildings.model'
@@ -10,6 +10,23 @@ export class BuildingsService {
     @InjectModel(Building) private buildingRepository: typeof Building,
     private authService: AuthService,
   ) {}
+
+  async getBuildingById(id) {
+    if (!id) {
+      throw new HttpException('Ошибка', HttpStatus.BAD_REQUEST)
+    }
+    const where = {
+      id: id
+    }
+
+    return await this.buildingRepository.findOne({
+      where: where
+    })
+  }
+
+  async getAllBuildings() {
+    return await this.buildingRepository.findAll()
+  }
 
   async addBuilding(data, token) {
     const userId = await this.authService.getUserIdByToken(token)
