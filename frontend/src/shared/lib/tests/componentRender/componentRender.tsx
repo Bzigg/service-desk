3a285e4fc4d6aea416a1e43da14ce6cb9db689f7
plekhanova <plekhanova@tochka.com>
2024-11-08 15:37:12 +0500

    FE выпилил i18

--- frontend/src/shared/lib/tests/componentRender/componentRender.tsx
+++ frontend/src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
-import { I18nextProvider } from 'react-i18next';
-import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { ReducersMapObject } from '@reduxjs/toolkit';
@@ -22,9 +20,7 @@ export function componentRender(component: ReactNode, options: componentRenderOp
     return render(
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
-                <I18nextProvider i18n={i18nForTests}>
-                    {component}
-                </I18nextProvider>
+                {component}
             </StoreProvider>
         </MemoryRouter>,
     );
